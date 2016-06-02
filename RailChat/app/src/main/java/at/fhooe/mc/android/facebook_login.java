0b842c04011c986fcf4cd9280fbff4433080a4c6
Jordan Pichler <Jordan@Jordans-MacBook-Pro.local>
2016-06-02 12:35:05 +0200

    first settings for facebook-Login implementation

---
+++ RailChat/app/src/main/java/at/fhooe/mc/android/facebook_login.java
@@ -0,0 +1,13 @@
+package at.fhooe.mc.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class facebook_login extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_facebook_login);
+    }
+}
