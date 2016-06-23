76e23b74cea5abfc7e0862fcca600f06fc042665
Jordan Pichler <j.pichler@icloud.com>
2016-06-23 22:16:43 +0200

    - redesigned Navigation Drawer -- User specific data (Name, Profile Pic) is now displayed in drawer header

--- app/src/main/java/at/fhooe/mc/android/main_menu/MainMenu.java
+++ app/src/main/java/at/fhooe/mc/android/main_menu/MainMenu.java
@@ -1,8 +1,14 @@
 package at.fhooe.mc.android.main_menu;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawable;
+import android.support.v4.graphics.drawable.RoundedBitmapDrawableFactory;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -12,8 +18,20 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.request.target.BitmapImageViewTarget;
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.ui.email.SignInActivity;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import at.fhooe.mc.android.R;
+import at.fhooe.mc.android.login.LoginSplash;
 
 public class MainMenu extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -42,6 +60,30 @@ public class MainMenu extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        //insert user Data to Drawer Header
+        View drawerTop = navigationView.getHeaderView(0);
+        TextView userName = (TextView) drawerTop.findViewById(R.id.nav_header_username);
+        TextView userEmail = (TextView) drawerTop.findViewById(R.id.nav_header_email);
+        final ImageView userPhoto = (ImageView) drawerTop.findViewById(R.id.nav_header_userphoto);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+        if (user != null) {
+            userName.setText(user.getDisplayName());
+            userEmail.setText("Hallo!");
+            if (user.getPhotoUrl() != null) {
+                Glide.with(this).load(user.getPhotoUrl()).asBitmap().centerCrop().into(new BitmapImageViewTarget(userPhoto) {
+                    @Override
+                    protected void setResource(Bitmap resource) {
+                        RoundedBitmapDrawable circularBitmapDrawable =
+                                RoundedBitmapDrawableFactory.create(MainMenu.this.getResources(), resource);
+                        circularBitmapDrawable.setCircular(true);
+                        userPhoto.setImageDrawable(circularBitmapDrawable);
+                    }
+                });
+            }
+        }
+
     }
 
     @Override
@@ -93,7 +135,16 @@ public class MainMenu extends AppCompatActivity
         } else if (id == R.id.nav_settings) {
 
         } else if (id == R.id.nav_signout) {
-            
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+                            // user is now signed out
+                            Intent i = new Intent(MainMenu.this, LoginSplash.class);
+                            startActivity(i);
+                            finish();
+                        }
+                    });
         }
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
