82c9e1cb1081a24275fd73c2293fd9e27ffe83ab
Qre4Tor <Bastian.Maringer@gmx.net>
2016-07-05 17:29:25 +0200

    Boards
    blabla

---
+++ app/src/main/java/at/fhooe/mc/android/board/Board.java
@@ -0,0 +1,147 @@
+package at.fhooe.mc.android.board;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+
+import java.util.Calendar;
+
+import at.fhooe.mc.android.R;
+import at.fhooe.mc.android.main_menu.MainMenu;
+
+public class Board extends AppCompatActivity implements View.OnClickListener {
+
+    protected DatabaseReference myRef_Board;
+    protected BoardAdapter adapter;
+    protected ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board);
+
+//        BoardAdapter adapter = new BoardAdapter(this);
+//        adapter.add(new BoardData("ÖBB is voll super",null,"Basti"));
+//        setListAdapter(adapter);
+
+
+        adapter = new BoardAdapter(this);
+        myRef_Board = MainMenu.database.getDatabase().getReference();
+        buildBoard();
+
+        listView = (ListView)findViewById(R.id.board_list);
+        if (listView != null) listView.setAdapter(adapter);
+        else Toast.makeText(this,"Adapter ist null",Toast.LENGTH_SHORT).show();
+
+        Button b;
+        b = (Button)findViewById(R.id.board_button_add);
+        b.setOnClickListener(this);
+
+    }
+
+    private void buildBoard() {
+        FirebaseUser user = com.google.firebase.auth.FirebaseAuth.getInstance().getCurrentUser();
+        final String userID = user.getUid();
+        final boolean[] inTrain = {false};
+        final String[] rjID = new String[]{""};
+
+        final Calendar[] date = {Calendar.getInstance()};
+        int day = date[0].get(Calendar.DAY_OF_MONTH);
+        int month = date[0].get(Calendar.MONTH);
+        int year = date[0].get(Calendar.YEAR);
+        String s = day + "-" + (month + 1) + "-" + year;
+
+//        final DatabaseReference ref_RJ = myRef_Board.child("RailjetTraveler").child(s);
+//        ref_RJ.addValueEventListener(new ValueEventListener() {
+//            @Override
+//            public void onDataChange(DataSnapshot dataSnapshot) {
+//                if (!dataSnapshot.exists()) Toast.makeText(Board.this,"You are not logged in to a travel so far! Please log in to a Railjet!",Toast.LENGTH_SHORT).show();
+//                else {
+//                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+////                        BoardData data = postSnapshot.getValue(BoardData.class);
+////                        adapter.add(data);
+//                        String train = postSnapshot.getKey();
+//                        DatabaseReference ref_Users = ref_RJ.child(train);
+//                        ref_Users.addValueEventListener(new ValueEventListener() {
+//                            @Override
+//                            public void onDataChange(DataSnapshot dataSnapshot) {
+//                                if (!dataSnapshot.exists())
+//                                    Toast.makeText(Board.this, "No users in Railchat found!", Toast.LENGTH_SHORT).show();
+//                                else {
+//                                    for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+//                                        if (userID.equals(postSnapshot.getValue())) {
+//                                            inTrain[0] = true;
+//                                            break;
+//                                        }
+//                                    }
+//                                }
+//                            }
+//                            @Override
+//                            public void onCancelled(DatabaseError databaseError) {
+//                                System.out.println(databaseError.getMessage());
+//                            }
+//                        });
+//                        if (inTrain[0]) {
+//                            rjID[0] = train;
+//                            break;
+//                        }
+//                    }
+//                }
+//            }
+//            @Override
+//            public void onCancelled(DatabaseError databaseError) {
+//                System.out.println(databaseError.getMessage());
+//            }
+//        });
+//
+//        if (inTrain[0]) {
+//            myRef_Board.child("Boards").child(rjID[0]).addValueEventListener(new ValueEventListener() {
+//                @Override
+//                public void onDataChange(DataSnapshot snapshot) {
+//
+//                    //Gets called at beginning and at a change
+//                    //System.out.println("There are " + snapshot.getChildrenCount() + " blog posts");
+//
+//                    if (snapshot.getChildrenCount() > 0) {
+//                        //Clear the view
+//                        adapter.clear();
+//                        for (DataSnapshot postSnapshot: snapshot.getChildren()) {
+//                            BoardData data = postSnapshot.getValue(BoardData.class);
+//                            adapter.add(data);
+//                        }
+//                    }else {
+//                        Toast.makeText(Board.this,"No posts on board! Post something yourself!",Toast.LENGTH_SHORT).show();
+//                    }
+//
+//
+//                }
+//
+//                @Override
+//                public void onCancelled(DatabaseError databaseError) {
+//                    System.out.println("The read failed: " + databaseError.getMessage());
+//                }
+//            });
+//        }
+
+    }
+
+    @Override
+    public void onClick(View _v) {
+        switch (_v.getId()){
+            case R.id.board_button_add : {
+                Intent i = new Intent(this,NewItem.class);
+                startActivity(i);
+            }break;
+            default : {
+                Toast.makeText(this,"ClickListener no id found",Toast.LENGTH_SHORT).show();
+            } break;
+        }
+    }
+}
---
+++ app/src/main/java/at/fhooe/mc/android/board/NewItem.java
@@ -0,0 +1,61 @@
+package at.fhooe.mc.android.board;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.firebase.database.DatabaseReference;
+
+import at.fhooe.mc.android.R;
+import at.fhooe.mc.android.main_menu.MainMenu;
+
+/**
+ * Created by Bastian on 05.07.2016.
+ */
+public class NewItem extends AppCompatActivity implements View.OnClickListener {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_board_new);
+
+        Button b;
+        b = (Button)findViewById(R.id.board_new_button_add);
+        b.setOnClickListener(this);
+        b = (Button)findViewById(R.id.board_new_button_back);
+        b.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View _v) {
+        switch (_v.getId()) {
+            case R.id.board_new_button_add : {
+                EditText title = (EditText)findViewById(R.id.board_new_user_title);
+                String s = title.getText().toString();
+//                FirebaseAuth auth = FirebaseAuth.getInstance();
+//                String user = auth.getCurrentUser().getDisplayName();
+                BoardData data = new BoardData(s, com.google.firebase.auth.FirebaseAuth.getInstance().getCurrentUser());
+
+
+                DatabaseReference newRef = MainMenu.database.getDatabase().getReference();
+                newRef = newRef.child("Boards").push();//.setValue(data);
+                data.setID(newRef.getKey());
+                newRef.setValue(data);
+
+                Toast.makeText(this, "BoardItem was successfully added", Toast.LENGTH_SHORT).show();
+                setContentView(R.layout.activity_board);
+
+                finish();
+            } break;
+            case R.id.board_new_button_back : {
+                finish();
+            } break;
+            default: {
+                Toast.makeText(this,"Buton Click not found",Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+}
