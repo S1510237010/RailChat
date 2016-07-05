ff8fa19ee118844a9dd55c0dd26a82ba8e48afb8
Jordan Pichler <j.pichler@icloud.com>
2016-07-05 19:12:11 +0200

    - login tweaks (bug when signing out and signing back in in MainMenu)

--- app/src/main/java/at/fhooe/mc/android/main_menu/MainMenu.java
+++ app/src/main/java/at/fhooe/mc/android/main_menu/MainMenu.java
@@ -56,7 +56,7 @@ public class MainMenu extends AppCompatActivity
         setSupportActionBar(toolbar);
 
         mInstance = FirebaseAuth.getInstance();
-        Log.i("LOGIN", "got auth Object");
+        Log.i("LOGIN", "onCreate(): got auth Object");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
@@ -76,9 +76,8 @@ public class MainMenu extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-        if (mInstance.getCurrentUser() != null) {
-            Toast.makeText(MainMenu.this, "User != null", Toast.LENGTH_SHORT).show();
-            Log.i("LOGIN", "User != null, no need to launch Longin UI");
+
+            Log.i("LOGIN", "onCreate(): User != null");
 
             //insert user Data to Drawer Header
             View drawerTop = navigationView.getHeaderView(0);
@@ -89,7 +88,7 @@ public class MainMenu extends AppCompatActivity
             FirebaseUser user = mInstance.getCurrentUser();
             if (user != null) {
                 userName.setText(user.getDisplayName());
-                userEmail.setText("Hallo!");
+                userEmail.setText("Hello");
                 if (user.getPhotoUrl() != null) {
                     Glide.with(this).load(user.getPhotoUrl()).asBitmap().centerCrop().into(new BitmapImageViewTarget(userPhoto) {
                         @Override
@@ -101,11 +100,13 @@ public class MainMenu extends AppCompatActivity
                         }
                     });
                 }
-            }
-        } else {
-            launchSignIn();
 
-        }
+                //Upload User data to separate Database-Object to allow communication
+                database.getDatabase().getReference("Users").child(user.getUid()).child("Name").setValue(user.getDisplayName());
+                database.getDatabase().getReference("Users").child(user.getUid()).child("Photo").setValue(user.getPhotoUrl());
+
+            }
+          
 
 
     }
@@ -194,8 +195,8 @@ public class MainMenu extends AppCompatActivity
                             public void onComplete(@NonNull Task<Void> task) {
                                 // user is now signed out
                                 //refresh Activity
-                                launchSignIn();
                                 finish();
+                                launchSignIn();
                             }
                         });
             }
